703a0c3b75297dbb68a030157f3997d69b42897c
johnlaw <peter.john.llp@gmail.com>
2024-04-14 23:21:03 +0800

    Add more info

--- Proc_Pretrained_Model/Prepare3b_OverView_Training_dataset.py
+++ Proc_Pretrained_Model/Prepare3b_OverView_Training_dataset.py
@@ -3,10 +3,12 @@ import pickle
 import pandas as pd
 
 def show_eng_df_info(size):
-    fileObj = open(var.FILE_TRAINING_ENG_NOS + str(size) + ".obj", 'rb')
+    file_name = var.FILE_TRAINING_ENG_NOS + str(size) + ".obj"
+    fileObj = open(file_name, 'rb')
     df = pickle.load(fileObj)
     fileObj.close()
     print("-------------------------")
+    print("File Name: " + file_name)
     print(type(df))
     print("Size: " + str(len(df)))
     emotionList = df[var.COLUMN_NAME_STD_LABEL].tolist()
@@ -16,10 +18,12 @@ def show_eng_df_info(size):
     print("-------------------------")
 
 def show_multi_df_info(size):
-    fileObj = open(var.FILE_TRAINING_MUL_NOS + str(size) + ".obj", 'rb')
+    file_name = var.FILE_TRAINING_MUL_NOS + str(size) + ".obj"
+    fileObj = open(file_name, 'rb')
     df = pickle.load(fileObj)
     fileObj.close()
     print("-------------------------")
+    print("File Name: " + file_name)
     print(type(fileObj))
 
     print("Size: " + str(len(df)))
@@ -34,11 +38,12 @@ def show_multi_df_info(size):
     print("-------------------------")
 
 
-
+print("====== English ======")
 for size in var.sampling_size_list:
     print("Work on " + str(size))
     show_eng_df_info(size)
 
+print("====== Multilingual ======")
 for size in var.sampling_size_list:
     print("Work on " + str(size))
     show_multi_df_info(size)
