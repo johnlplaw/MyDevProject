1d897197e6a96d5dc5fdbd025cac0cf9a1487ef8
johnlaw <peter.john.llp@gmail.com>
2024-08-09 02:19:10 +0800

    Add analysis module for predicting by the models

--- Proc_Analysis/Lib_analysis.py
+++ Proc_Analysis/Lib_analysis.py
@@ -1,6 +1,6 @@
 import commons.mysql.mysqlHelper as sqlHelper
 import mysql.connector
-from transformers import BertTokenizer, BertForSequenceClassification
+from transformers import BertTokenizer, BertForSequenceClassification, XLMRobertaTokenizer,XLMRobertaForSequenceClassification
 import torch
 ###################
 # Model types
@@ -9,8 +9,22 @@ MODEL_MBERT_MUL = "mbert_mul"
 MODEL_XLMR_ENG = "xlmr_eng"
 MODEL_XLMR_MUL = "xlmr_mul"
 
-NUM_CLASSES = 6
+# Column name for redicted result
+COL_MBERT_ENG_400 = "mbert_eng_400"
+COL_MBERT_ENG_1600 = "mbert_eng_1600"
+COL_MBERT_ENG_2800 = "mbert_eng_2800"
+COL_MBERT_MUL_400 = "mbert_mul_400"
+COL_MBERT_MUL_1600 = "mbert_mul_1600"
+COL_MBERT_MUL_2800 = "mbert_mul_2800"
+COL_XLMR_ENG_400 = "xlmr_eng_400"
+COL_XLMR_ENG_1600 = "xlmr_eng_1600"
+COL_XLMR_ENG_2800 = "xlmr_eng_2800"
+COL_XLMR_MUL_400 = "xlmr_mul_400"
+COL_XLMR_MUL_1600 = "xlmr_mul_1600"
+COL_XLMR_MUL_2800 = "xlmr_mul_2800"
 
+
+NUM_CLASSES = 6
 
 EMOTION_LABEL_NEUTRAL_CODE = 0
 EMOTION_LABEL_HAPPY_CODE = 1
@@ -46,6 +60,10 @@ Label_Code_Desc = {
 ###################
 
 class TextData:
+    """
+    TextData class to store the tweets info.
+    """
+
     id = ""
     clean_text = ""
     pseudo_label = ""
@@ -62,13 +80,18 @@ class TextData:
 
 
 def get_cleanned_ori_text():
+    """
+    Query the cleanned text info.
+    :return:
+    """
+
     print("Start to query ...")
     TextDataList = []
     try:
         conn = sqlHelper.get_mysql_conn()
         mycursor = conn.cursor()
         Select_sql = """
-                select id, clean_text, pseudo_label from tweets where length(clean_text) > 0 limit 0, 100
+                select id, clean_text, pseudo_label from tweets where length(clean_text) > 0 limit 0, 5
                 """
         mycursor.execute(Select_sql)
         result = mycursor.fetchall()
@@ -94,9 +117,17 @@ def get_cleanned_ori_text():
 
 
 def evaluation(textList, tokenizer, model):
+    """
+    Generate the predicted emotion for the texts.
+    :param textList: The passed in texts list
+    :param tokenizer: The tokenizer of the model
+    :param model: The model object
+    :return: The textList with predicted emotion
+    """
+
     predicted_list = []
     for text in textList:
-        print(text.id)
+
         # Tokenize input text
         inputs = tokenizer(text.clean_text, return_tensors='pt')
 
@@ -106,25 +137,107 @@ def evaluation(textList, tokenizer, model):
 
         # Get predicted label (assuming binary classification)
         predicted_label = torch.argmax(outputs.logits, dim=1).item()
-        print(predicted_label)
         text.setPredictedLabel(predicted_label)
         predicted_list.append(text)
 
     return predicted_list
 
 
-def prepare_model_mBERT_Eng():
+def prepare_model_mBERT():
     model_name = "bert-base-multilingual-uncased"
     tokenizer = BertTokenizer.from_pretrained(model_name)
     model = BertForSequenceClassification.from_pretrained(model_name, num_labels=NUM_CLASSES)
     return model, tokenizer
 
+def prepare_model_xlmr():
+    model_name = "FacebookAI/xlm-roberta-base"
+    tokenizer = XLMRobertaTokenizer.from_pretrained(model_name)
+    model = XLMRobertaForSequenceClassification.from_pretrained(model_name, num_labels=NUM_CLASSES)
+    return model, tokenizer
 
-#
-# theDataList = get_cleanned_ori_text()
-# for disp in theDataList:
-#     print(str(disp.id) + " / " + disp.clean_text+ " / " + disp.pseudo_label )
+def prediction_mbert(selected_Model, modelPath, textList):
+    """
+    Perform predcition by mBERT
+    :param selected_Model: The model object
+    :param modelPath: The model file path
+    :param textList: The pass in text list
+    :return: The text list with predicted info
+    """
+
+    # Prepare the model, tokenizer
+    print("Prepare model and tokenizer ... start")
+    model, tokenizer = prepare_model_mBERT()
+    print("Prepare model and tokenizer ... end")
+
+    # Load the model
+    print("Load model ... start")
+    state_dict = torch.load(modelPath + selected_Model.fileName)
+    model.load_state_dict(state_dict)
+    print("Load model ... end")
+
+    # 5. Set the model to evaluation mode
+    model.eval()
+
+    # 6. Predictin process
+    predicted_datalist = evaluation(textList, tokenizer, model)
+    return predicted_datalist
+
+def prediction_xlmr(selected_Model, modelPath, data_list):
+    """
+    Perform predcition by FB-XLM-R
+    :param selected_Model: The model object
+    :param modelPath: The model file path
+    :param textList: The pass in text list
+    :return: The text list with predicted info
+    """
+
+    # Prepare the model, tokenizer
+    print("Prepare model and tokenizer ... start")
+    model, tokenizer = prepare_model_xlmr()
+    print("Prepare model and tokenizer ... end")
+
+    # Load the model
+    print("Load model ... start")
+    state_dict = torch.load(modelPath + selected_Model.fileName)
+    model.load_state_dict(state_dict)
+    print("Load model ... end")
+
+    # 5. Set the model to evaluation mode
+    model.eval()
+
+    # 6. Predictin process
+    predicted_datalist = evaluation(data_list, tokenizer, model)
+    return predicted_datalist
 
 
+def update_prediction_result(textDataList, colname):
+    """
+    Update the predicted result in the tweets table
+    :param textDataList: The pass in text data list with predicted label
+    :param colname: the column name to be updated
+    :return: Nothing
+    """
+    try:
+        conn = sqlHelper.get_mysql_conn()
+        mycursor = conn.cursor()
+        update_sql = "Update tweets set " + colname + "= %s where id = %s"
 
+        values = []
+
+        for textData in textDataList:
+            tuppleData = (textData.id, textData.predicted_label)
+            values.append(tuppleData)
+
+        # executemany() method
+        mycursor.executemany(update_sql, values)
+        # save changes
+        conn.commit()
+
+    except mysql.connector.Error as error:
+        print("Failed to insert record to database: {}".format(error))
+    finally:
+        if conn.is_connected():
+            mycursor.close()
+            conn.close()
+            print("MySQL connection is closed")
 
---
+++ Proc_Analysis/Proc_Analysis_Each_Model.py
@@ -0,0 +1,53 @@
+import Lib_analysis as lib
+import os.path
+import torch
+
+class Selected_Model:
+    def __init__(self, fileName, modelName, colName):
+        self.fileName = fileName
+        self.modelName = modelName
+        self.colName = colName
+
+
+#1.  Get the texts to be predicted
+data_list = lib.get_cleanned_ori_text()
+
+# for txtData in data_list:
+#     print(str(txtData.id) + " - " + txtData.clean_text)
+
+#2. identify the selected model
+e_mBert_400 = Selected_Model("Chk-mBERT_English_400_1e-05_model.pth", "mBERT-400", lib.COL_MBERT_ENG_400)
+e_mBert_1600 = Selected_Model("Chk-mBERT_English_1600_1e-05_model.pth", "mBERT-1600", lib.COL_MBERT_ENG_1600)
+e_mBert_2800 = Selected_Model("Chk-mBERT_English_2800_1e-05_model.pth", "mBERT-2800", lib.COL_MBERT_ENG_2800)
+e_fbXLMr_400 = Selected_Model("Chk-FB-XLM-R_English_400_1e-05_model.pth", "FB-XLM-R-400", lib.COL_XLMR_ENG_400)
+e_fbXLMr_1600 = Selected_Model("Chk-FB-XLM-R_English_400_1e-05_model.pth", "FB-XLM-R-1600", lib.COL_XLMR_ENG_1600)
+e_fbXLMr_2800 = Selected_Model("Chk-FB-XLM-R_English_400_1e-05_model.pth", "FB-XLM-R-2800", lib.COL_XLMR_ENG_2800)
+m_mBert_400 = Selected_Model("Chk-mBERT_Multilingual_400_1e-05_model.pth", "mBERT-400", lib.COL_MBERT_MUL_400)
+m_mBert_1600 = Selected_Model("Chk-mBERT_Multilingual_1600_1e-05_model.pth", "mBERT-1600", lib.COL_MBERT_MUL_1600)
+m_mBert_2800 = Selected_Model("Chk-mBERT_Multilingual_2800_1e-05_model.pth", "mBERT-2800", lib.COL_MBERT_MUL_2800)
+m_fbXLMr_400 = Selected_Model("Chk-FB-XLM-R_Multilingual_400_1e-05_model.pth", "FB-XLM-R-400", lib.COL_XLMR_MUL_400)
+m_fbXLMr_1600 = Selected_Model("Chk-FB-XLM-R_Multilingual_400_1e-05_model.pth", "FB-XLM-R-1600", lib.COL_XLMR_MUL_1600)
+m_fbXLMr_2800 = Selected_Model("Chk-FB-XLM-R_Multilingual_400_1e-05_model.pth", "FB-XLM-R-2800", lib.COL_XLMR_MUL_2800)
+
+
+mbert_model_list = [e_mBert_400, e_mBert_1600, e_mBert_2800, m_mBert_400, m_mBert_1600, m_mBert_2800]
+xmlr_model_list = [e_fbXLMr_400, e_fbXLMr_1600, e_fbXLMr_2800, m_fbXLMr_400, m_fbXLMr_1600, m_fbXLMr_2800]
+
+model_file_path = "../Proc_Pretrained_Model/output/"
+
+for model in mbert_model_list:
+    isFound = os.path.exists(model_file_path + model.fileName)
+    print(isFound)
+    predict_list = lib.prediction_mbert(model, model_file_path, data_list)
+    print("===")
+    print(len(predict_list))
+    print("===")
+
+for model in xmlr_model_list:
+    isFound = os.path.exists(model_file_path + model.fileName)
+    print(isFound)
+    predict_list = lib.prediction_xlmr(model, model_file_path, data_list)
+    print("===")
+    print(len(predict_list))
+    print("===")
+#3
